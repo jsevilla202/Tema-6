f09240b823d603e03754034edd2616ee452c625e
meganano202 <meganano202@192.168.1.134>
2023-02-20 12:07:27 +0100

    Ej 5 y 6 hecho

---
+++ Tema 6/src/ejecicios/Ej5.java
@@ -0,0 +1,30 @@
+package ejecicios;
+
+import java.util.Scanner;
+
+public class Ej5 {
+
+	public static void main(String[] args) {
+		String frase="";
+		String palabra;
+		boolean fin = false;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		while(fin == false) {
+			System.out.print("Inserta la palabra: ");
+			palabra = sc.next();
+			if(palabra.compareToIgnoreCase("fin") != 0) {
+				frase += palabra;
+				frase += " ";
+			}
+			else {
+				fin = true;
+			}
+		}
+		
+		System.out.println("Su frase es: \n" + frase);
+
+	}
+
+}
---
+++ Tema 6/src/ejecicios/Ej6.java
@@ -0,0 +1,32 @@
+package ejecicios;
+
+import java.util.Scanner;
+
+public class Ej6 {
+
+	public static void main(String[] args) {
+		String frase;
+		String buscar;
+		String[] palabras;
+		int contador = 0;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Inserte una frase en la que buscar: ");
+		frase = sc.nextLine();
+		System.out.print("Inserte una palabra para buscar: ");
+		buscar = sc.next();
+		
+		palabras = frase.split(" ");
+		
+		for(int i = 0; i<palabras.length; i++) {
+			if(palabras[i].equals(buscar)) {
+				contador++;
+			}
+		}
+		
+		System.out.println("La palabra aparece un total de: " + contador + " veces");
+
+	}
+
+}
